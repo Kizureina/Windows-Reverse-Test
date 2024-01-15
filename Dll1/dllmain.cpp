1f79bf70e889bd3dcfa7c6c5de995d38d75a6be9
Kizureina <houchangkun@gmail.com>
2024-01-15 17:51:33 +0800

    DLL注入

--- Dll1/dllmain.cpp
+++ Dll1/dllmain.cpp
@@ -1,24 +1,38 @@
 ﻿#include <Windows.h>
 
-BOOL APIENTRY DllMain( HMODULE hModule,
-                       DWORD  ul_reason_for_call,
-                       LPVOID lpReserved
-                     )
+
+VOID WINAPI NewThreadFunc(LPVOID lpParamate)
 {
-    switch (ul_reason_for_call)
-    {
-    case DLL_PROCESS_ATTACH:
-        break;
-
-    case DLL_THREAD_ATTACH:
-        break;
-
-    case DLL_THREAD_DETACH:
-        break;
-
-    case DLL_PROCESS_DETACH:
-        break;
-    }
-    return TRUE;
+	MessageBox(NULL, L"NewThreadFunc", NULL, NULL);
 }
 
+
+extern "C" VOID __declspec(dllexport) MyFunc()
+{
+
+}
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+		case DLL_PROCESS_ATTACH:
+			MessageBoxW(NULL, L"DLL_PROCESS_ATTACH", NULL, NULL);
+			CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)NewThreadFunc, NULL, NULL, NULL);
+			break;
+		case DLL_THREAD_ATTACH:
+			MessageBoxW(NULL, L"DLL_THREAD_ATTACH", NULL, NULL);
+			break;
+		case DLL_THREAD_DETACH:
+			MessageBoxW(NULL, L"DLL_THREAD_DETACH", NULL, NULL);
+			break;
+		case DLL_PROCESS_DETACH:
+			MessageBoxW(NULL, L"DLL_PROCESS_DETACH", NULL, NULL);
+			break;
+	}
+	return TRUE;
+}
---
+++ dll_injection_test/dll_injection_test.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+int main()
+{
+	//LoadLibraryW(L"Dll1.dll");
+	MessageBoxW(NULL, L"LoadLibraryW", NULL, NULL);
+
+	std::cout << "Hello World!\n";
+}
